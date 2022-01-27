9973616ef373c181d1823b568fcd694d533fad79
Nemesis <szakacsmpaula@gmail.com>
2022-01-27 11:14:10 +0100

    get changes

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const musicContainer = document.getElementById('music-container');
-const playBtn = document.getElementById('play');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
-
-const audio = document.getElementById('audio');
-const progress = document.getElementById('progress');
-const progressContainer = document.getElementById('progress-container');
-const title = document.getElementById('title');
-const cover = document.getElementById('cover');
-const currTime = document.querySelector('#currTime');
-const durTime = document.querySelector('#durTime');
+class MusicPlayer {
+  constructor() {
+	this.musicContainer = document.getElementById("music-container");
+	this.playBtn = document.getElementById("play");
+	this.prevBtn = document.getElementById("prev");
+	this.nextBtn = document.getElementById("next");
+	
+	this.audio = document.getElementById("audio");
+	this.progress = document.getElementById("progress");
+	this.progressContainer = document.getElementById("progress-container");
+	this.title = document.getElementById("title");
+	this.cover = document.getElementById("cover");
+	this.currTime = document.querySelector("#currTime");
+	this.durTime = document.querySelector("#durTime");
+  }
+}
 
 
 
 // Song titles
-const songs = ['hey', 'prp', 'beat01'];
+const songs = ["hey", "prp", "beat01"];
 
 // Keep track of song
 let songIndex = 2;
@@ -23,32 +27,32 @@ let songIndex = 2;
 loadSong(songs[songIndex]);
 
 // Update song details
-function loadSong(song) {
+ loadSong(song) {
   title.innerText = song;
   audio.src = `music/${song}.mp3`;
   cover.src = `images/prp.jpg`;
 }
 
 // Play song
-function playSong() {
-  musicContainer.classList.add('play');
-  playBtn.querySelector('i.fas').classList.remove('fa-play');
-  playBtn.querySelector('i.fas').classList.add('fa-pause');
+playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
 
   audio.play();
 }
 
 // Pause song
-function pauseSong() {
-  musicContainer.classList.remove('play');
-  playBtn.querySelector('i.fas').classList.add('fa-play');
-  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+ pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
 
   audio.pause();
 }
 
 // Previous song
-function prevSong() {
+ prevSong() {
   songIndex--;
 
   if (songIndex < 0) {
@@ -61,7 +65,7 @@ function prevSong() {
 }
 
 // Next song
-function nextSong() {
+ nextSong() {
   songIndex++;
 
   if (songIndex > songs.length - 1) {
@@ -74,14 +78,14 @@ function nextSong() {
 }
 
 // Update progress bar
-function updateProgress(e) {
+ updateProgress(e) {
   const { duration, currentTime } = e.srcElement;
   const progressPercent = (currentTime / duration) * 100;
   progress.style.width = `${progressPercent}%`;
 }
 
 // Set progress bar
-function setProgress(e) {
+setProgress(e) {
   const width = this.clientWidth;
   const clickX = e.offsetX;
   const duration = audio.duration;
@@ -90,71 +94,64 @@ function setProgress(e) {
 }
 
 //get duration & currentTime for Time of song
-function DurTime (e) {
-	const {duration,currentTime} = e.srcElement;
-	var sec;
-	var sec_d;
+ DurTime(e) {
+  const { duration, currentTime } = e.srcElement;
+  var sec;
+  var sec_d;
 
-	// define minutes currentTime
-	let min = (currentTime==null)? 0:
-	 Math.floor(currentTime/60);
-	 min = min <10 ? '0'+min:min;
+  // define minutes currentTime
+  let min = currentTime == null ? 0 : Math.floor(currentTime / 60);
+  min = min < 10 ? "0" + min : min;
 
-	// define seconds currentTime
-	function get_sec (x) {
-		if(Math.floor(x) >= 60){
-			
-			for (var i = 1; i<=60; i++){
-				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
-					sec = Math.floor(x) - (60*i);
-					sec = sec <10 ? '0'+sec:sec;
-				}
-			}
-		}else{
-		 	sec = Math.floor(x);
-		 	sec = sec <10 ? '0'+sec:sec;
-		 }
-	} 
+  // define seconds currentTime
+   get_sec(x) {
+    if (Math.floor(x) >= 60) {
+      for (var i = 1; i <= 60; i++) {
+        if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+          sec = Math.floor(x) - 60 * i;
+          sec = sec < 10 ? "0" + sec : sec;
+        }
+      }
+    } else {
+      sec = Math.floor(x);
+      sec = sec < 10 ? "0" + sec : sec;
+    }
+  }
 
-	get_sec (currentTime,sec);
+  get_sec(currentTime, sec);
 
-	// change currentTime DOM
-	currentTime.innerHTML = min +':'+ sec;
+  // change currentTime DOM
+  currentTime.innerHTML = min + ":" + sec;
 
-	// define minutes duration
-	let min_d = (isNaN(duration) === true)? '0':
-		Math.floor(duration/60);
-	 min_d = min_d <10 ? '0'+min_d:min_d;
+  // define minutes duration
+  let min_d = isNaN(duration) === true ? "0" : Math.floor(duration / 60);
+  min_d = min_d < 10 ? "0" + min_d : min_d;
 
+  get_sec_d(x) {
+    if (Math.floor(x) >= 60) {
+      for (var i = 1; i <= 60; i++) {
+        if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+          sec_d = Math.floor(x) - 60 * i;
+          sec_d = sec_d < 10 ? "0" + sec_d : sec_d;
+        }
+      }
+    } else {
+      sec_d = isNaN(duration) === true ? "0" : Math.floor(x);
+      sec_d = sec_d < 10 ? "0" + sec_d : sec_d;
+    }
+  }
 
-	 function get_sec_d (x) {
-		if(Math.floor(x) >= 60){
-			
-			for (var i = 1; i<=60; i++){
-				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
-					sec_d = Math.floor(x) - (60*i);
-					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
-				}
-			}
-		}else{
-		 	sec_d = (isNaN(duration) === true)? '0':
-		 	Math.floor(x);
-		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
-		 }
-	} 
+  // define seconds duration
 
-	// define seconds duration
-	
-	get_sec_d (duration);
+  get_sec_d(duration);
 
-	// change duration DOM
-	duration.innerHTML = min_d +':'+ sec_d;
-		
-};
+  // change duration DOM
+  duration.innerHTML = min_d + ":" + sec_d;
+}
 
 // Event listeners
-playBtn.addEventListener('click', () => {
-  const isPlaying = musicContainer.classList.contains('play');
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
 
   if (isPlaying) {
     pauseSong();
@@ -164,17 +161,17 @@ playBtn.addEventListener('click', () => {
 });
 
 // Change song
-prevBtn.addEventListener('click', prevSong);
-nextBtn.addEventListener('click', nextSong);
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
 
 // Time/song update
-audio.addEventListener('timeupdate', updateProgress);
+audio.addEventListener("timeupdate", updateProgress);
 
 // Click on progress bar
-progressContainer.addEventListener('click', setProgress);
+progressContainer.addEventListener("click", setProgress);
 
 // Song ends
-audio.addEventListener('ended', nextSong);
+audio.addEventListener("ended", nextSong);
 
 // Time of song
-audio.addEventListener('timeupdate',DurTime);
+audio.addEventListener("timeupdate", DurTime);
