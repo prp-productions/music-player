73957cc02e65ed4d3d221fa39297269f3d97ad86
Nemesis <szakacsmpaula@gmail.com>
2022-01-27 11:47:45 +0100

    copying

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -16,4 +16,65 @@ export class MusicPlayer {
   display() {
     this.musicContainer.innerHTML = `music`;
   }
+
+  loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/prp.jpg`;
+  }
+
+  playSong() {
+    musicContainer.classList.add("play");
+    playBtn.querySelector("i.fas").classList.remove("fa-play");
+    playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+    audio.play();
+  }
+
+  pauseSong() {
+    musicContainer.classList.remove("play");
+    playBtn.querySelector("i.fas").classList.add("fa-play");
+    playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+    audio.pause();
+  }
+
+  prevSong() {
+    songIndex--;
+
+    if (songIndex < 0) {
+      songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+  }
+
+  nextSong() {
+    songIndex++;
+
+    if (songIndex > songs.length - 1) {
+      songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+  }
+  
+  updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+  
+  setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+  
+    audio.currentTime = (clickX / width) * duration;
+  }
+
 }
