1d89f3745beddb65651919545c8b820ffae1fd03
Nemesis <szakacsmpaula@gmail.com>
2022-01-27 12:05:11 +0100

    all code copied

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -12,6 +12,25 @@ export class MusicPlayer {
     this.cover = document.getElementById("cover");
     this.currTime = document.querySelector("#currTime");
     this.durTime = document.querySelector("#durTime");
+
+    this.playBtn.addEventListener("click", () => {
+      const isPlaying = musicContainer.classList.contains("play");
+
+      if (isPlaying) {
+        this.pauseSong();
+      } else {
+        this.playSong();
+      }
+    });
+
+    this.prevBtn.addEventListener("click", this.prevSong);
+    this.nextBtn.addEventListener("click", this.nextSong);
+
+    this.audio.addEventListener("timeupdate", this.updateProgress);
+    this.progressContainer.addEventListener("click", this.setProgress);
+    this.audio.addEventListener("ended", this.nextSong);
+
+    this.audio.addEventListener("timeupdate", this.durTime);
   }
   display() {
     this.musicContainer.innerHTML = `music`;
@@ -39,7 +58,7 @@ export class MusicPlayer {
     audio.pause();
   }
 
-  prevSong() {
+  prevSong = () => {
     songIndex--;
 
     if (songIndex < 0) {
@@ -49,9 +68,9 @@ export class MusicPlayer {
     loadSong(songs[songIndex]);
 
     playSong();
-  }
+  };
 
-  nextSong() {
+  nextSong = () => {
     songIndex++;
 
     if (songIndex > songs.length - 1) {
@@ -61,20 +80,74 @@ export class MusicPlayer {
     loadSong(songs[songIndex]);
 
     playSong();
-  }
-  
-  updateProgress(e) {
+  };
+
+  updateProgress = (e) => {
     const { duration, currentTime } = e.srcElement;
     const progressPercent = (currentTime / duration) * 100;
     progress.style.width = `${progressPercent}%`;
   }
-  
-  setProgress(e) {
+
+  setProgress = (e) => {
     const width = this.clientWidth;
     const clickX = e.offsetX;
     const duration = audio.duration;
-  
+
     audio.currentTime = (clickX / width) * duration;
   }
 
+  durTime = (e) =>{
+    const { duration, currentTime } = e.srcElement;
+    var sec;
+    var sec_d;
+
+    // define minutes currentTime
+    let min = currentTime == null ? 0 : Math.floor(currentTime / 60);
+    min = min < 10 ? "0" + min : min;
+
+    // define seconds currentTime
+    function get_sec(x) {
+      if (Math.floor(x) >= 60) {
+        for (var i = 1; i <= 60; i++) {
+          if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+            sec = Math.floor(x) - 60 * i;
+            sec = sec < 10 ? "0" + sec : sec;
+          }
+        }
+      } else {
+        sec = Math.floor(x);
+        sec = sec < 10 ? "0" + sec : sec;
+      }
+    }
+
+    get_sec(currentTime, sec);
+
+    // change currentTime DOM
+    currentTime.innerHTML = min + ":" + sec;
+
+    // define minutes duration
+    let min_d = isNaN(duration) === true ? "0" : Math.floor(duration / 60);
+    min_d = min_d < 10 ? "0" + min_d : min_d;
+
+    function get_sec_d(x) {
+      if (Math.floor(x) >= 60) {
+        for (var i = 1; i <= 60; i++) {
+          if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+            sec_d = Math.floor(x) - 60 * i;
+            sec_d = sec_d < 10 ? "0" + sec_d : sec_d;
+          }
+        }
+      } else {
+        sec_d = isNaN(duration) === true ? "0" : Math.floor(x);
+        sec_d = sec_d < 10 ? "0" + sec_d : sec_d;
+      }
+    }
+
+    // define seconds duration
+
+    get_sec_d(duration);
+
+    // change duration DOM
+    duration.innerHTML = min_d + ":" + sec_d;
+  }
 }
